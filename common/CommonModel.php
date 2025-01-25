404e70e500f1508c77c4b1dcfc280fefe9062fc1
ep1088 <ep1088@qq.com>
2025-01-25 11:03:12 +0800

    cs-fixer

--- common/CommonModel.php
+++ common/CommonModel.php
@@ -1,14 +1,18 @@
 <?php
+
+declare(strict_types=1);
+
 namespace common;
 
 /**
  * 通用model基类
  */
-class CommonModel extends \myphp\Model {
+class CommonModel extends \myphp\Model
+{
     //public static $resetOption = false; //sql组合项执行后是否重置
     //public static $resetWhere = false; //使用->where是否重置之前的条件
 
     //状态
-    const STATUS_OK = 1;
-    const STATUS_NO = 0;
+    public const STATUS_OK = 1;
+    public const STATUS_NO = 0;
 }

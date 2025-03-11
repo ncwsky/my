9af018c642b92f603503a22c8f8c88f7723b3efd
ep1088 <ep1088@qq.com>
2025-03-11 11:43:36 +0800

    1

---
+++ common/TraitControl.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace common;
+
+
+use myphp\Control;
+use myphp\Value;
+
+trait TraitControl
+{
+    /**
+     * 临时文件上传 成功处理后移动 /index/tmp-upload
+     * 使用原始内容上传 /index/tmp-upload?raw=1&file_name=test.jpg  post  raw
+     * 使用原始base64内容上传 /index/tmp-upload?raw=base64&file_name=test.jpg  post  base64(raw)
+     * @return array
+     */
+    public function tmpUpload()
+    {
+        $upload = \Upload::getInstance();
+        $upload->fileSize = 5;
+        $upload->uploadPath = ROOT_DIR . '/tmp/'; //设置文件相对的上传路径
+        if (Q('tmp') == 'no') {
+            $upload->uploadPath = ROOT_DIR . '/up/y' . date('Y/md/');
+        }
+        $upload->fileType = $upload->fileType . ',ico';
+        $upload->realPath = SITE_WEB . $upload->uploadPath; //设置文件的绝对路径
+        $upload->imgOptimize = true;
+        $isEditor = isset($_GET['editor']) ? (bool)$_GET['editor'] : null;
+        $name = Q('name', 'files');
+        if ($isEditor) $name = 'upfile';
+
+        $raw = Q('get.raw');
+        if ($raw) {
+            $_FILES[$name] = [
+                "tmp_name" => \myphp::rawBody(),
+                "name" => Q('get.file_name'),
+                "size" => 0,
+                "error" => UPLOAD_ERR_OK,
+                "type" => "raw_string",
+            ];
+            if ($raw == 'base64') {
+                $str = substr($_FILES[$name]['tmp_name'], 0, 32);
+                if ($pos = strpos($str, ';base64,')) { //带有base64前缀的截取处理
+                    $_FILES[$name]['tmp_name'] = substr($_FILES[$name]['tmp_name'], $pos + 8);
+                    //未指定文件名时自动处理
+                    if ($_FILES[$name]['name'] === '' && preg_match('/^data:\s*(\w+)\/(\w+);base64,/', $str, $result)) {
+                        $_FILES[$name]['name'] = uniqid() . '.' . $result[2];
+                    }
+                }
+                $_FILES[$name]['tmp_name'] = base64_decode($_FILES[$name]['tmp_name']);
+            }
+            $_FILES[$name]['size'] = strlen($_FILES[$name]['tmp_name']);
+        }
+
+        $data = $upload->upload($name);
+        $picErr = [];
+        $pics = [];
+        if (isset($data[0]['state'])) {//多个文件上传
+            for ($i = 0; $i < count($data); $i++) {
+                if ($data[$i]['state'] == '1') {
+                    $pics[$i] = $data[$i]['url'];
+                } else {
+                    $picErr[] = "[" . ($i + 1) . "]" . ($data[$i]['title'] ? "【" . $data[$i]['title'] . "】" : '') . $data[$i]['state'];
+                }
+            }
+        } else {//单个文件上传
+            if ($data['state'] == '1') {
+                $pics[] = $data['url'];
+            } else {
+                $picErr[] = ($data['title'] ? "【" . $data['title'] . "】" : '') . $data['state'];
+            }
+        }
+        if (empty($pics)) { //&& $picErr
+            if ($isEditor) {
+                return ['state' => $picErr ? implode(" \n", $picErr) : '上传失败', 'url' => ''];
+            }
+            return Control::fail(implode(" \n", $picErr));
+        }
+
+        if ($isEditor) {
+            /**
+             * 得到上传文件所对应的各个参数,数组结构
+             * array(
+             *     "state" => "",          //上传状态，上传成功时必须返回"SUCCESS"  或失败信息
+             *     "url" => "",            //返回的地址
+             *     "title" => "",          //新文件名
+             *     "original" => "",       //原始文件名
+             *     "type" => ""            //文件类型
+             *     "size" => "",           //文件大小
+             * )
+             */
+            if (count($pics) > 1) {
+                $list = [];
+                foreach ($pics as $pic) {
+                    $list[] = ['url' => $pic];
+                }
+                return ['state' => 'SUCCESS', 'list' => $list];
+            }
+            return ['state' => 'SUCCESS', 'url' => $pics[0], 'type' => '', 'original' => ''];
+        }
+
+        return Control::ok($pics);
+    }
+
+    //生成随机验证码 /index/captcha[?name=captcha]
+    public function captcha()
+    {
+        $name = Value::get($_GET, 'name', ['min' => 1, 'max' => 30], 'captcha');
+        $w = isset($_GET['w']) ? (int)($_GET['w']) : 120;
+        $h = isset($_GET['h']) ? (int)($_GET['h']) : 36;
+        $size = isset($_GET['size']) ? (int)($_GET['size']) : 18;
+        $len = isset($_GET['len']) ? (int)($_GET['len']) : 4;
+        $type = isset($_GET['number']) ? (int)($_GET['number']) : 0;
+
+        \Image::$outputString = true;
+
+        if (!empty($_GET['raw'])) { //返回sid用于cookie处理
+            $raw = \Image::code($w, $h, $size, $len, $type, function ($code) use ($name) {
+                session($name, strtolower($code));
+            });
+            $raw = \myphp\Session::getId() . ':' . $raw;
+            return Control::html($raw);
+        }
+
+        \myphp::setHeader("Content-Type", "image/png");
+        return \Image::code($w, $h, $size, $len, $type, function ($code) use ($name) {
+            session($name, strtolower($code));
+        });
+    }
+}
---
+++ common/Weixin.php
@@ -0,0 +1,154 @@
+<?php
+namespace common;
+
+use EasyWeChat\Factory;
+use myphp\Helper;
+
+class Weixin
+{
+    use \MyMsg;
+    //openid for work
+    const OP_DEV = 'o8E801u7dC30OOOjGEwO7fE-EOmM';
+
+    #后台非公司网络登陆时微信验证
+    public static $admOuterNetWxAuth = [self::OP_DEV];
+
+    const WX_CFG = 'wxp';
+
+    /**
+     * @var \EasyWeChat\OfficialAccount\Application[]
+     */
+    public static $app = [];
+    public static $wxAsync= true; //微信异步通知 默认异步
+
+    /**
+     * @param string $name
+     * @return \EasyWeChat\OfficialAccount\Application
+     */
+    public static function getApp($name = self::WX_CFG)
+    {
+        if (!isset(self::$app[$name])) {
+            self::$app[$name] = Factory::officialAccount(GetC($name));
+        }
+        return self::$app[$name];
+    }
+
+    public static function clog($msg)
+    {
+        Helper::toFileLog(__DIR__ . '/../log/Weixin-' . date("Ym") . '.log', $msg);
+    }
+
+    /**
+     * 通用模板消息发送
+     * @param string $openid
+     * @param string $template_id
+     * @param array $sendData
+     * @param string $url
+     * @param string $cfg
+     * @return array|bool|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function pushCommon($openid, $template_id, $sendData, $url = '', $cfg = self::WX_CFG)
+    {
+        try {
+            $app = static::getApp($cfg);
+            $data = [
+                'touser' => $openid,
+                'template_id' => $template_id,
+                'data' => $sendData,
+            ];
+            if ($url) {
+                $data['url'] = $url;
+            }
+            return $app->template_message->send($data);
+        } catch (\Exception $e) {
+            static::clog('file:' . $e->getFile() . 'line:' . $e->getLine() . ',code:' . $e->getCode() . ',msg:' . $e->getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 生成临时二维码
+     * @param int|string $sceneVal int[32位非0整型], string[1-64]
+     * @param int $time 最长30【2592000】天
+     * @param string $cfg
+     * @return array|bool
+     */
+    public static function qr($sceneVal, $time = 610, $cfg = self::WX_CFG)
+    {
+        $app = static::getApp($cfg);
+        try {
+            $qrRet = $app->qrcode->temporary($sceneVal, $time);
+            if (!isset($qrRet["url"])) {
+                throw new \Exception(toJson($qrRet));
+            }
+            return [
+                "url" => $qrRet["url"],
+                "imageUrl" => $app->qrcode->url($qrRet["ticket"]),
+            ];
+
+            #return $qrRet; //[ticket,expire_seconds,url]
+        } catch (\Exception $e) {
+            self::clog('创建二维码失败：' . $e->getMessage());
+            return self::err('创建二维码失败：' . $e->getMessage());
+        }
+        #return $app->qrcode->temporary($sceneVal, $time);
+    }
+
+    /**
+     * 生成永久二维码
+     * @param int|string $sceneVal int[1-100000], string[1-64]
+     * @param string $cfg
+     * @return array|\EasyWeChat\Kernel\Support\Collection|object|\Psr\Http\Message\ResponseInterface|string
+     */
+    public static function foreverQr($sceneVal, $cfg = self::WX_CFG)
+    {
+        $app = static::getApp($cfg);
+        try {
+            $qrRet = $app->qrcode->forever($sceneVal); //[ticket,url]
+            if (!isset($qrRet["url"])) {
+                throw new \Exception(toJson($qrRet));
+            }
+            return [
+                "url" => $qrRet["url"],
+                "imageUrl" => $app->qrcode->url($qrRet["ticket"]),
+            ];
+            #return $qrRet;
+        } catch (\Exception $e) {
+            return self::err('创建二维码失败：' . $e->getMessage());
+        }
+        #return $app->qrcode->forever($sceneVal); //[ticket,url]
+    }
+
+    const AES_KEY = 'fEhrTmbbQI';
+
+    //用于请求识别有效性
+    public static function enAesJson($data, $expiry = 0)
+    {
+        if (!is_scalar($data)) $data = toJson($data);
+        return base64_encode(\AES::encode(sprintf('%010d', $expiry ? $expiry + time() : 0) . $data, self::AES_KEY));
+    }
+
+    public static function deAesJson($data)
+    {
+        $data = \AES::decode(base64_decode($data), self::AES_KEY);
+        $time = (int)substr($data, 0, 10);
+        if ($time == 0 || $time - time() > 0) {
+            return json_decode(substr($data, 10), true);
+        }
+        return null;
+    }
+
+    //微信消息提醒
+    public static function notice($title, $msg, $remark = '', $openid = [self::OP_DEV])
+    {
+        $apiUrl = 'https://api.guanliyuangong.com/default/notice';
+        $data = [
+            'openid' => is_array($openid) ? implode(',', $openid) : $openid,
+            'title' => $title,
+            'msg' => $msg,
+            'remark' => $remark,
+        ];
+        return \Http::doPost($apiUrl, base64_encode(Helper::rc4(Helper::toJson($data), GetC('rc4_key'))), 10, ['Accept:*/*']);
+    }
+}

37d39890767194efd4c8c46bb1bbb2c274bc01f4
ep1088 <ep1088@qq.com>
2025-03-11 11:42:51 +0800

    1

--- common/Auth.php
+++ common/Auth.php
@@ -5,14 +5,31 @@ declare(strict_types=1);
 namespace common;
 
 //后台用户验证类
-use common\model\Admin;
+use module\admin\model\Admin;
+use module\admin\model\Role;
 use myphp\Helper;
 
-class Auth
+class AdminAuth extends \myphp\BaseAuth
 {
     use \MyMsg;
 
+    /**
+     * @var AdminAuth
+     */
+    private static $instance;
     private $roleRow = [];
+
+    public static function getInstance(): AdminAuth
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    public static function getPurview($roleId = 0): string
+    {
+        return self::getInstance()->roleData($roleId)['purview'] ?? '';
+    }
 
     public function __construct()
     {
@@ -33,96 +50,36 @@ class Auth
 
     /**
      * 获取指定角色的权限数据
-     * @param int $roleId
-     * @return mixed
-     * @throws \Exception
+     * @param int $roleId //todo 后期考虑多角色处理权限
+     * @return array
      */
-    public function roleData($roleId = 0)
+    public function roleData(int $roleId = 0): array
     {
         $roleId = $roleId ?: (int)session('adminRole');
         if (!isset($this->roleRow[$roleId])) {
-            $this->roleRow[$roleId] = db()->getOne('select id,rank,name,purview from `role` where `id`=' . $roleId);
+            $this->roleRow[$roleId] = Role::fields('rank,name,purview')->where(['id' => $roleId])->one();
+            if ($this->roleRow[$roleId]) {
+                $purview = strtr($this->roleRow[$roleId]['purview'], ';', ',');
+                //组合配置的权限设置 角色.角色编号
+                if (isset(\myphp::$cfg['role'][$roleId])) {
+                    $purview = $purview . ',' . \myphp::$cfg['role'][$roleId];
+                }
+                $this->roleRow[$roleId]['purview'] = $purview;
+            } else {
+                $this->roleRow[$roleId] = [
+                    'rank' => 0,
+                    'name' => '-',
+                    'purview' => ''
+                ];
+            }
         }
         return $this->roleRow[$roleId];
     }
 
-    /**
-     * 权限验证
-     * @param string|null $control 单独调用时设置的控制器名称
-     * @param string|null $action 设置为空字符时仅验证控制器
-     * @param array|null $get 附加参数的权限验证
-     * @return bool
-     * @throws \Exception
-     */
-    public function chkPurview($control = null, $action = null, $get = null)
-    {
-        $roleId = (int)session('adminRole');
-        $row = $this->roleData($roleId);
-        if (empty($row)) {
-            return self::err('用户所属角色无效!');
-        }
-
-        $control === null && cookie('adminRoleName', $row['name']); //角色名
-        $purview = json_decode($row['purview'], true);
-        //组合配置的权限设置 角色.角色编号
-        if (isset(\myphp::$cfg['role'][$row['id']])) {
-            $purview = array_merge($purview, \myphp::$cfg['role'][$row['id']]);
-        }
-
-        if (!is_array($purview)) {
-            return self::err('用户所属角色没有权限记录信息!');
-        }
-        //超级管理员
-        if (isset($purview['isadmin'])) {
-            return true;
-        }
-        if ($control === null) { //获得控制器名
-            $control = strtolower(\myphp::$env['c']);
-        }
-        if (!isset($purview[$control])) {
-            return self::err('用户所属角色没有' . $control . '的权限!');
-        }
-        if ($purview[$control] === 1) { #拥有此模块所有权限
-            return true;
-        }
-        if ($action === null) {
-            $action = strtolower(\myphp::$env['a']); //获得方法名
-        } elseif ($action === '') { //自定义传值 仅验证控制器
-            return true;
-        }
-
-        if (!isset($purview[$control][$action])) {
-            return self::err('用户所属角色没有' . $control . '/' . $action . '操作的权限!');
-        }
-
-        $err = '用户所属角色没有' . $control . '/' . $action . '操作的权限!';
-        if ($purview[$control][$action] == '!') { //支持 参数值为“!”表示没有此权限
-            return self::err($err);
-        }
-        //指定参数的权限验证 只支持一个参数的验证  一般是在自定权限中才会设置
-        if ($purview[$control][$action] !== 1) { // hy=1 或 hy-1
-            $para = strtr($purview[$control][$action], '-', '=');
-            if (strpos($para, '=')) {
-                [$_k, $_v] = explode('=', $para);
-                if ($get === null) {
-                    if (isset($_GET[$_k]) && $_GET[$_k] == $_v) {
-                        return true;
-                    }
-                } else {
-                    if (isset($get[$_k]) && $get[$_k] == $_v) {
-                        return true;
-                    }
-                }
-            }
-            return self::err($err);
-        }
-        return true;
-    }
-
     //检测用户是否登陆
-    public function adminLogin()
+    public function adminLogin(): bool
     {
-        return session('adminId');
+        return (bool)session('adminId');
     }
 
     public function adminCheck()
